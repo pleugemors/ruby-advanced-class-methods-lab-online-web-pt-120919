64f28521d5733a5fd13b82549e9e9717be2b388f
Paul Leugemors <pleugemors@yahoo.com>
2019-12-11 12:05:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,8 +47,8 @@ class Song
   def self.new_from_filename(file_name)
     parts = file_name.split(".")
     data = parts[0].split(" - ")
-    puts data[1]
     song = self.new_by_name(data[1])
     song.artist_name = data[0]
+    song 
   end
 end
